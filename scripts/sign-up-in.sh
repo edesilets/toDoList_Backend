951c61ac60802129e24fdc138f4bda7418037ffc
Ethan <erdesi90562@gmail.com>
2016-02-28 17:29:18 -0500

    bash does sign up and sign in

---
+++ scripts/sign-up-in.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+URL='http://localhost:3000'
+
+echo 'json here is good :'
+curl ${URL}/sign-up \
+  --silent \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "test",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+  }'
+
+echo '----------------------------------------------'
+echo  'sign IN.....'
+echo '----------------------------------------------'
+
+curl http://localhost:3000/sign-in \
+  --silent \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "test",
+      "password": "password"
+    }
+  }' | jsonlint
