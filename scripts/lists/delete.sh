12ec75c432a4dfd6d94b4a57e86ae52e232cd62c
Ethan <erdesi90562@gmail.com>
2016-03-05 13:33:38 -0500

    create curl to delete a list and all items attached to that list

---
+++ scripts/lists/delete.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TOKEN='d04dc1ee80d33f4cd62e59c83f3536ab'
+URL='http://localhost:3000'
+#URL="http://httpbin.org/post"
+
+echo '-----------------------------------------'
+
+curl ${URL}/lists/6 \
+  --silent \
+  --request DELETE \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}"
+echo
+echo '-----------------------------------------'
+echo
